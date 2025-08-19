e2afc68953aa4de8c47b5ad7411f29f3b4f7a476
npragin <npragin@gmail.com>
2025-08-18 22:26:15 -0400

    Update favicon.

--- pages/_document.js
+++ pages/_document.js
@@ -4,7 +4,7 @@ export default function Document() {
 	return (
 		<Html lang="en">
 			<Head>
-				<link rel="icon" href="/favicon.ico" sizes="any" />
+				<link rel="icon" href="/favicon.svg" sizes="any" />
 			</Head>
 			<body>
 				<Main />
