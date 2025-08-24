fca46ec0d917994d24209ef805d50d56054372e6
npragin <npragin@gmail.com>
2025-08-24 02:06:07 -0400

    Update blurb #17

--- constants/index.js
+++ constants/index.js
@@ -2,7 +2,8 @@ export const NAME = "Noah Pragin"
 
 export const DESIGNATION = "C.S. @ Oregon State University"
 
-export const BLURB = "I build cloud-based client-facing applications and dive head-first into new technology."
+export const BLURB = "I build autonomous systems for robots spanning defense, agriculture, and racing."
+// export const BLURB = "I build cloud-based client-facing applications and dive head-first into new technology."
 
 export const SOCIAL_LINKS = {
 	GMAIL: "mailto:npragin@gmail.com",
