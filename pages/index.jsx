130c37ed69c39383c1a08fb0fc9cc20e516a7e8f
npragin <npragin@gmail.com>
2025-08-23 23:03:57 -0400

    Lint

--- pages/index.jsx
+++ pages/index.jsx
@@ -3,15 +3,15 @@ import { MyExperience } from "@components"
 import { About } from "@components"
 
 const home = () => {
-  return (
-    <div className="Home-Page -z-10 -">
-      <Head>
-        <title>Noah Pragin</title>
-      </Head>
-      <About />
-      <MyExperience/>
-    </div>
-  )
+	return (
+		<div className="Home-Page -z-10 -">
+			<Head>
+				<title>Noah Pragin</title>
+			</Head>
+			<About />
+			<MyExperience/>
+		</div>
+	)
 }
 
 export default home
