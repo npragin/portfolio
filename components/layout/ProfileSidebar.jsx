fbf7c4bad558d5fd38303e3f76549cebf96691a4
npragin <npragin@gmail.com>
2025-08-24 13:24:59 -0400

    Apply styles based on active section

--- components/layout/ProfileSidebar.jsx
+++ components/layout/ProfileSidebar.jsx
@@ -19,16 +19,31 @@ const ProfileSidebar = ({ activeSection }) => {
 			{/* Middle content */}
 			<nav className="nav grow overflow-y-auto" aria-label="In-page jump links">
 				<ul className="mt-16 w-max">
-					{["about", "experience"].map((section) => (
+					{["about", "experience"].map((section) => {
+					const isActive = activeSection === section
+
+					return (
 						<li key={section}>
 							<a className="group flex items-center py-3" href={`#${section}`}>
-								<span className="nav-indicator mr-4 h-px w-8 bg-slate-600 transition-all group-hover:w-16 group-hover:bg-slate-200 group-focus-visible:w-16 group-focus-visible:bg-slate-200 motion-reduce:transition-none"></span>
-								<span className="nav-text text-xs font-bold uppercase tracking-widest text-slate-500 group-hover:text-slate-200 group-focus-visible:text-slate-200">
+								<span className={`
+									mr-4 h-px transition-all duration-300 motion-reduce:transition-none
+									${isActive
+										? 'w-16 !bg-slate-200' 
+										: 'w-8 bg-slate-600 group-hover:w-16 group-hover:bg-slate-200 group-focus-visible:w-16 group-focus-visible:bg-slate-200'
+									}
+									`}></span>
+								<span className={`
+										text-xs font-bold uppercase tracking-widest transition-all duration-300
+										${isActive 
+											? 'text-slate-200' 
+											: 'text-slate-500 group-hover:text-slate-200 group-focus-visible:text-slate-200'
+										}
+									`}>
 									{section}
 								</span>
 							</a>
 						</li>
-					))}
+					)})}
 				</ul>
 			</nav>
 
