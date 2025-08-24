c49a2f8d1da010cb672a3a2ae61f9d54e1abb1e0
npragin <npragin@gmail.com>
2025-08-24 02:34:29 -0400

    Migrate experience API to Constants

--- components/experience/MyExperience.jsx
+++ components/experience/MyExperience.jsx
@@ -4,25 +4,16 @@ import axios from "axios"
 import { LuArrowUpRight as ArrowUpRight } from "react-icons/lu"
 import { ExperienceCard } from "@components"
 import { ParagraphSkeleton } from "@components"
+import { experience } from "/constants"
 
 const MyExperience = () => {
-	const { isLoading, data } = useQuery({
-		queryKey: ["experience"],
-		queryFn: () => axios.get("api/experience").then(({ data }) => data),
-	})
-
 	return (
 		<>
 			<div className="grid justify-items-center grid-flow-row m-1 gap-4 px-2 md:px-8">
 				{
-					isLoading ?
-						[1, 2, 3, 4, 5, 6].map((i) => (
-							<ParagraphSkeleton key={i} className={"space-y-2 p-8"} />
-						))
-						:
-						data?.map((data, key) => (
-							<ExperienceCard key={key} data={data} />
-						))
+					experience.map((data, key) => (
+						<ExperienceCard key={key} data={data} />
+					))
 				}
 			</div>
 			<div className="flex ml-10 mt-8 pb-24">
