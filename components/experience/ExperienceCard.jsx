08c3333403360ca310fae9c04c94226de4488570
npragin <npragin@gmail.com>
2025-12-16 14:41:11 -0800

    Add project cards

--- components/experience/ExperienceCard.jsx
+++ components/experience/ExperienceCard.jsx
@@ -1,58 +1,22 @@
-import { React, useState } from "react"
-import Link from "next/link"
-import { LuArrowUpRight as ArrowUpRight } from "react-icons/lu"
-import { CardLayout, Tools } from "@components"
+import React from "react"
+import BaseCard from "../common/cards/BaseCard"
 
 const ExperienceCard = ({ data }) => {
-	const [isHovered, setIsHovered] = useState(false)
-
-	const cardContent = (
-		<div
-			className="flex h-full space-y-2 p-4 pt-1 card_stylings relative"
-			onMouseEnter={() => setIsHovered(true)}
-			onMouseLeave={() => setIsHovered(false)}
-		>
-			{data.link && (
-				<div className="absolute top-0 right-0 p-4 transition-all">
-					<ArrowUpRight
-						className={`transition-all ${
-							isHovered ? "text-violet-500 translate-x-1 -translate-y-1" : "text-slate-200"
-						}`}
-						size={20}
-					/>
-				</div>
-			)}
-			<div className="w-[25%] justify-center p-2 m-1">
-				<div className="text-slate-500 text-sm">{data.date}</div>
-			</div>
-			<div className="w-[75%] justify-center p-2">
-				<div className={`text-md tracking-wide transition-colors ${isHovered && data.link ? "text-violet-500" : "text-slate-200"}`}>
-					{`${data.title} • ${data.company}`}
-				</div>
-				{data.previousTitles &&
-                    <div className="text-slate-400 text-md tracking-wide">{data.previousTitles}</div>
-				}
-				<div className="text-sm text-slate-400 font-normal pt-3">
-					{data.desc}
-				</div>
-				{data.tools &&
-                    <Tools tools={data.tools}/>
-				}
-			</div>
-		</div>
+	const leftSection = (
+		<div className="text-slate-500 text-sm">{data.date}</div>
 	)
 
-	if (data.link) {
-		return (
-			<Link href={data.link} target="_blank" rel="noopener noreferrer">
-				<CardLayout className="cursor-pointer">
-					{cardContent}
-				</CardLayout>
-			</Link>
-		)
-	}
+	const titleContent = `${data.title} • ${data.company}`
 
-	return <CardLayout>{cardContent}</CardLayout>
+	return (
+		<BaseCard
+			data={data}
+			leftSection={leftSection}
+			titleContent={titleContent}
+			previousTitles={data.previousTitles}
+			toolsKey="tools"
+		/>
+	)
 }
 
 export default ExperienceCard
