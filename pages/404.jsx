d9f547115bcc37ab9a6aa528cdfab91bba6b4be8
npragin <npragin@gmail.com>
2025-12-21 00:19:21 -0800

    Don't use jsx tag

--- pages/404.jsx
+++ pages/404.jsx
@@ -4,14 +4,6 @@ import { BaseLayout } from "@components"
 const NotFound = () => {
 	return (
 		<BaseLayout>
-			<style jsx global>{`
-				.next-error-h1 {
-					font-weight: 400 !important;
-				}
-				.next-error-h1 + div h2 {
-					font-weight: 300 !important;
-				}
-			`}</style>
 			<div className="min-h-screen bg-primary text-slate-400">
 				<Error statusCode={404} />
 			</div>
