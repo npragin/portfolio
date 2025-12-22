ca3fb6452d10f2ee8bff2732439729b98d5caf88
npragin <npragin@gmail.com>
2025-12-22 13:59:40 -0800

    Add vercel speed and visitor analytics

--- components/layout/BaseLayout.jsx
+++ components/layout/BaseLayout.jsx
@@ -1,8 +1,11 @@
 import { SpotlightCursor } from "@components"
+import { Analytics, SpeedInsights } from "@vercel/analytics/react"
 
 const BaseLayout = ({ children, className = "" }) => {
 	return (
 		<>
+			<Analytics />
+			<SpeedInsights />
 			<SpotlightCursor />
 			<div className={`min-h-screen font-sans ${className}`}>
 				{children}
